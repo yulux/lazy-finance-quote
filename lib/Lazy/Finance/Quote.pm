642fd54a5c672f01d079d5904fe42e289ad1c9a0
limodev <limodev@yulux.de>
2016-06-15 13:30:49 +0000

    Command line script for query api directly and tidy things

--- lib/Lazy/Finance/Quote.pm
+++ lib/Lazy/Finance/Quote.pm
@@ -6,24 +6,23 @@ use Mojo::UserAgent;
 use XML::Simple qw(:strict);
 use Hash::Flatten qw(:all);
 
-our $VERSION  = '0.01';
+our $VERSION = '0.01';
 
-my $prov_info = {
-  yahoo => { url => 'http://query.yahooapis.com/v1/public' },
-};
+my $prov_info = {yahoo => {url => 'http://query.yahooapis.com/v1/public'},};
 
 sub new { bless {}, shift }
 
 my $xmls = XML::Simple->new(ForceArray => 1, KeepRoot => 1, KeyAttr => []);
-my $ua   = Mojo::UserAgent->new;
+my $ua = Mojo::UserAgent->new;
 
 sub query {
   my ($self, $symbol) = @_;
-  my $sql_url = Mojo::URL->new('http://query.yahooapis.com/v1/public/yql')->query(
+  my $sql_url
+    = Mojo::URL->new('http://query.yahooapis.com/v1/public/yql')->query(
     q   => "select * from yahoo.finance.quotes where symbol in (\"$symbol\")",
     env => "store://datatables.org/alltableswithkeys"
-  );
-  my $xml = $ua->get($sql_url)->res->content->asset->slurp;
+    );
+  my $xml    = $ua->get($sql_url)->res->content->asset->slurp;
   my $xml_ds = $xmls->XMLin($xml);
   ## my $flat_hash = flatten($xml_ds);
   ## return $flat_hash if $flat_hash;
--- t/02.data.structure.t
+++ t/02.data.structure.t
@@ -7,15 +7,16 @@ use JSON::XS;
 use lib "../lib";
 require "$FindBin::Bin/../script/lazyfq";
 
-my $default_symbol = 'AAPL';
+my $default_symbol   = 'AAPL';
 my $default_provider = 'yahoo';
 
 # HTML/XML
 my $t = Test::Mojo->new;
-my $x = decode_json($t->get_ok('/finance/yahoo/AAPL')->tx->res->content->asset->slurp);
+my $x = decode_json(
+  $t->get_ok('/finance/yahoo/AAPL')->tx->res->content->asset->slurp);
 
-ok($x->{symbol} eq $default_symbol, $x->{symbol});
-ok($x->{time_stamp} =~ qr/\d+/, "time: $x->{time_stamp}");
+ok($x->{symbol} eq $default_symbol,         $x->{symbol});
+ok($x->{time_stamp} =~ qr/\d+/,             "time: $x->{time_stamp}");
 ok($x->{provider} =~ qr/$default_provider/, $x->{provider});
 
 done_testing();
